95b47fddc693133514f5662a99698c3c199b3318
hvlawren <hvlawren@ncsu.edu>
2016-07-17 19:06:42 -0400

    Got Angular2 running in web server

---
+++ scripts/main.js
@@ -0,0 +1,4 @@
+"use strict";
+const platform_browser_dynamic_1 = require('@angular/platform-browser-dynamic');
+const app_testcomponent_1 = require('./app.testcomponent');
+platform_browser_dynamic_1.bootstrap(app_testcomponent_1.AppComponent);
